75bc35a8ad34bafcec030e92cc4b8621ba91f770
aksha <aksha@LAPTOP-GVR300KR>
2023-02-09 21:07:34 +0530

    First Commit...........

---
+++ src/DemoGit/FirstGit.java
@@ -0,0 +1,15 @@
+package DemoGit;
+
+public class FirstGit {
+
+	
+	public static void main(String[] args) {
+		
+		String str = "Akshay Shinde";
+		
+		for(int i=str.length()-1; i>=0; i--)
+		{
+			System.out.print(str.charAt(i));
+		}
+	}
+}
